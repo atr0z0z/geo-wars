b6d99bb8907038e90a34067163c7891e7bf36239
atr0z0z <artur.trubicin1@gmail.com>
2025-04-30 21:18:08 +0300

    d

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import json
 import random
+import uuid
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+
+rooms = {}
 
 with open('questions.json', encoding='utf-8') as f:
     QUESTIONS = json.load(f)
@@ -23,8 +28,39 @@ def get_question():
     question = random.choice(QUESTIONS)
     return jsonify(question)
 
+@socketio.on('create_room')
+def handle_create(data):
+    room_id = str(uuid.uuid4())[:6].upper()
+    username = data['username']
+    color = data['color']
+
+    rooms[room_id] = {
+        'players': [{ 'name': username, 'color': color, 'sid': request.sid }],
+        'map': {},  # например: {'CA': {'owner': 'Alice', 'color': '#ff0000'}}
+        'turn_index': 0
+    }
+
+    join_room(room_id)
+    emit('room_created', {'room_id': room_id, 'players': rooms[room_id]['players']}, room=request.sid)
+
+
+@socketio.on('join_room')
+def handle_join(data):
+    room_id = data['room_id']
+    username = data['username']
+    color = data['color']
+
+    if room_id in rooms:
+        rooms[room_id]['players'].append({ 'name': username, 'color': color, 'sid': request.sid })
+        join_room(room_id)
+        emit('player_joined', { 'players': rooms[room_id]['players'] }, room=room_id)
+    else:
+        emit('error', {'message': 'Комната не найдена'})
+
+
 import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port)
+
